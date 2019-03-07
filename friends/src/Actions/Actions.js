59a0b1d0cca696c78a4b44957688653790c66e67
Francis Chen <fncis.chen@gmail.com>
2019-03-07 15:26:25 -0800

    adding Actions - schematic

---
+++ friends/src/Actions/Actions.js
@@ -0,0 +1,6 @@
+export const SIGN_IN = "SIGN_IN";
+export const SIGN_OUT ="SIGN_OUT";
+export const FETCHING_DATA = "FETCHING_DATA";
+
+export const DELETE_FRIEND = "DELETE_FRIEND";
+export const ADD_FRIEND = "ADD_FRIEND";
