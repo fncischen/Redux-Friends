a107d2493f847c370804ba60a60239f24ed13917
Francis Chen <fncis.chen@gmail.com>
2019-03-08 01:01:10 -0800

    added components

---
+++ friends/src/Components/Loading.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export const Loading = props => {
+    return(
+        <div className="loading">
+        Loading...
+        </div>
+    )
+}
---
+++ friends/src/Router/PrivateRouter.js
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+const PrivateRouter = ({ component: Component, ...rest }) => (
+    <Route
+      {...rest}
+      render={props =>
+        localStorage.getItem("token") ? (
+          <Component {...props} />
+        ) : (
+          <Redirect to="/login" />
+        )
+      }
+    />
+  );
