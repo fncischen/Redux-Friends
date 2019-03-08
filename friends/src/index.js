b9feb1c6f288fb0b4fe171dbe2ddb9484a300412
Francis Chen <fncis.chen@gmail.com>
2019-03-08 00:47:29 -0800

    updated middleware to put in loggingInMiddleWare to allow token to be set on local storage

--- friends/src/index.js
+++ friends/src/index.js
@@ -20,7 +20,7 @@ import { SIGN_IN } from './Actions/Actions';
 
 const store = createStore(
     friendsReducer,
-    applyMiddleware(thunk, logger),
+    applyMiddleware(loggingInMiddleWare, thunk, logger),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 )
 
