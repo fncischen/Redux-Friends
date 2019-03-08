acfb96fef34da3288013d430a7c58ac7b90c7e6a
Francis Chen <fncis.chen@gmail.com>
2019-03-07 23:13:47 -0800

    added router dependencies

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+
+        <ul>
+            <li>
+              <Link to="/public">Public Page</Link>
+            </li>
+            <li>
+              <Link to="/protected">Protected Page</Link>
+            </li>
+        </ul>
         
-        <Route path="/public" component={FriendsList}/>
+        <Route path="/" component={Public}/>
         <Route path="/login" component={Login}/>
         
         <PrivateRoute path='/private' component={Loading} />
-        <ProtectedRoute path='/protected' component={FriendsList} />
+        <ProtectedRoute path='/friends' component={FriendsList} />
       </div>
     );
   }
